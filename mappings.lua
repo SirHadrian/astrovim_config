d3ccb7117cbadebb73c972c2ae9ae0e7fc4cb869
Toma Adrian <sirhadrian21@gmail.com>
2023-05-15 21:37:54 +0300

    neovide font size and scale resize

--- mappings.lua
+++ mappings.lua
@@ -6,34 +6,47 @@
 return {
   -- first key is the mode
   n = {
-   
-    ["<A-j>"] = {":bprevious<cr>", desc="Move to previous buffer"},
-    ["<A-k>"]={":bnext<cr>", desc="Move to next buffer"},
+    ["<C-=>"] = {
+      function()
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.25
+      end
+    },
+    ["<C-->"] = {
+      function()
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1 / 1.25
+      end
+    },
+
+    ["<A-j>"] = { ":bprevious<cr>", desc = "Move to previous buffer" },
+    ["<A-k>"] = { ":bnext<cr>", desc = "Move to next buffer" },
 
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    
+
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
     ["<leader>c"] = {
-        function()
-          local bufs = vim.fn.getbufinfo { buflisted = true }
-          require("astronvim.utils.buffer").close(0)
-          if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
-        end,
-        desc = "Close buffer",
-      },
+      function()
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        require("astronvim.utils.buffer").close(0)
+        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+      end,
+      desc = "Close buffer",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
